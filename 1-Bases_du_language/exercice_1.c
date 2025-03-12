929a6722fc3b0c2be4bf4a71f400de791a428b4a
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-12 17:14:12 +0100

    Exercice 2 Done.

--- 1-Bases_du_language/exercice_1.c
+++ 1-Bases_du_language/exercice_1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//1. Afficher "Hello, World!" à l'écran.
 int main() {
     printf("Hello, World!");
     return 0;
---
+++ 1-Bases_du_language/exercice_2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//2. Déclarer et afficher une variable entière.
+int main() {
+    int variable = 2;
+    printf("%d",variable);
+    return 0;
+}
