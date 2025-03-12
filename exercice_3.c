15bcf3526ca5c98488d4d26c3e1faca1ca59826f
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-12 17:24:19 +0100

    Exercice 3 Done.

---
+++ exercice_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//3. Lire un entier entré par l'utilisateur et l'afficher.
+int main() {
+    int integer;
+    printf("Type your number : ");
+    scanf("%d", &integer);
+    printf("Your number is : %d",integer);
+    return 0;
+}
