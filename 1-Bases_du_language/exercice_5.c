97625a662238cfc6a09a5daf26bfd1c6a806d1b5
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-12 18:09:21 +0100

    Exercice 5 Done.

---
+++ 1-Bases_du_language/exercice_5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//5. Convertir un entier en flottant et vice versa.
+int main() {
+    int x = 4.0; //Conversion implicite d'un float en entier
+    int y = 2; //Conversion explicite d'un entier en float
+    float z = (float) y;
+    printf("%d\n",x);
+    printf("%f",z);
+    return 0;
+}
