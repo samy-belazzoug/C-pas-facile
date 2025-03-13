fb5f308a5ab44899ab66353bdf493482396b5437
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-13 13:23:01 +0100

    Exercice 7 Done.

---
+++ 1-Bases_du_language/exercice_7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//7. Lire un caractère et afficher son code ASCII.
+int main() {
+    char x = 'A'; char y = 'B'; char z = 'C';
+    printf("%d\n",x);printf("%d\n",y);printf("%d\n",z);
+    return 0;
+}
