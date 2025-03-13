d881e8568c13b61f975a6e04d74201dfe151f237
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-13 13:54:08 +0100

    Exercice 10 Done. FIRST PHASE DONE IN LESS THAN A DAY YEEES

---
+++ 1-Bases_du_language/exercice_10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//10. Calculer la moyenne de trois nombres.
+int main() {
+    int x = 10;
+    int y = 24;
+    int z = 12;
+    float moyenne = (float) (x+y+z)/3;
+    printf("%f",moyenne);
+    return 0;
+}
