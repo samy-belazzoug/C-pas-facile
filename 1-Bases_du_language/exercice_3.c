c55c72ef3b175bd0526c58104577dbc59e4a095f
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-28 14:52:31 +0100

    Exercice 18 Done.

--- 1-Bases_du_language/exercice_3.c
+++ 1-Bases_du_language/exercice_3.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 //3. Lire un entier entré par l'utilisateur et l'afficher.
-int main() {
-    int integer;
+int main() 
+    {int integer;
     printf("Type your number : ");
     scanf("%d", &integer);
     printf("Your number is : %d",integer);
-    return 0;
-}
+    return 0;}
---
+++ 2-Structures-de-controle/exercice18.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+//18. Lire un mois (1-12) et afficher son nombre de jours.
+int main() {
+
+
+int mois = 2;
+switch (mois) 
+    {
+    case 1:
+        printf("%i",31);
+        break;
+    case 2:
+        int annee;printf("Année : ");scanf("%i",&annee);
+        if (annee % 4) {printf("%i",28);}
+        else {printf("%i",29);}
+        break;
+    case 3:
+        printf("%i",31);
+        break;
+    case 5:
+        printf("%i",31);
+        break;
+    case 7:
+        printf("%i",31);
+        break;
+    case 8:
+        printf("%i",31);
+        break;
+    case 10:
+        printf("%i",31);
+        break;
+    case 12:
+        printf("%i",31);
+        break;
+    default:
+        printf("%i",30);}
+    return 0;
+}
