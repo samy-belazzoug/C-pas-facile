cc4d392c79f940688bb5fecc9e522222513bb4b7
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-28 12:27:56 +0100

    Exercice 15 really Done.

--- 2-Structures-de-controle/exercice15.c
+++ 2-Structures-de-controle/exercice15.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 //15. Trouver la somme et la moyenne des nombres d’un tableau.
 int main() 
-    {int array[] = {2,4,5,7,8,9,10}; //tableau de 10 entiers max.
-    int length = sizeof(array) / sizeof(array[0]);
+    {int array[] = {2,4,5,7,8,9,9}; //tableau de 10 entiers max.
+    int length = sizeof(array) / sizeof(array[0]); //longueur du tableau
+    
     int i; int somme = 0;
     for (i = 0; i < length ;i++) 
     {somme += array[i];}
-    printf("%d",somme);
+    float moyenne = (float) somme / length;
+    printf("Somme : ");printf("%d\n",somme);
+    printf("Moyenne (arrondie): ");printf("%f",moyenne);
     return 0;}
