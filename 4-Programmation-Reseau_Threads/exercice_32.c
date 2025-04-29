359c669ca41c8f2c9630a450ce40462a5956db55
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-04-29 18:30:12 +0200

    Exercice 32 Done FR.

--- 4-Programmation-Reseau_Threads/exercice_32.c
+++ 4-Programmation-Reseau_Threads/exercice_32.c
@@ -2,6 +2,7 @@
 #include <winsock2.h>
 #include <stdio.h>
 #pragma comment(lib,"ws2_32.lib")
+#define DEFAULT_PORT "2000"
 
 /*Créer un socket TCP côté client
 Connecter un client à google.com sur le port 80 (sans rien envoyer).*/
@@ -9,15 +10,45 @@ Connecter un client à google.com sur le port 80 (sans rien envoyer).*/
 int main() {
 
     WSADATA wsaData;
-    int initializeWinsock;
-    initializeWinsock = WSAStartup(MAKEWORD(2,2),&wsaData);
+    int iResult;
+    iResult = WSAStartup(MAKEWORD(2,2),&wsaData);
 
-    if (initializeWinsock != 0) {
-        printf("Winsock failed : %d",initializeWinsock);
+    if (iResult != 0) {
+        printf("Winsock failed : %d",iResult);
         return 1;
     }
 
+    struct addrinfo *result = NULL,
+                    *ptr = NULL,
+                    hints;
+    
+    ZeroMemory(&hints, sizeof(hints)); //Set everything to zero to go on a clean base
+    hints.ai_family = AF_INET; //IP protocol : IPv4
+    hints.ai_socktype = SOCK_STREAM; //Socket type : stream
+    hints.ai_protocol = IPPROTO_TCP; //Protocol : TCP/IP
+    
+    //Resolve the server address and port
+    iResult = getaddrinfo("127.0.0.1",DEFAULT_PORT,&hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed : %d",iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    
+    ptr = result;
+
+    ConnectSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Error at socket() : %ld",WSAGetLastError());
+    freeaddrinfo(result);
+    closesocket(ConnectSocket);
+    WSACleanup;
+    return 1;
+    }
+
+    
     printf("Winsock initialized successfully !");
-
-
 }
